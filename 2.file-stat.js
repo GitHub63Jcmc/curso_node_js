082e07276e9309342491035084a45bb4a72d4c0f
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2025-07-30 01:59:37 +0200

    npm init, picocolors

--- 2.file-stat.js
+++ 2.file-stat.js
@@ -1,13 +1,11 @@
 // File sistem / información del sistema.
 const fs = require('node:fs') // a partir de Node 16, se recomienda poner node: antes de fs
 
-
-
 const stats = fs.statSync('./archivo.txt') // sincrono, bloquea el hilo de ejecución
 console.log(`Hola Joao ${stats} `) // muestra información del archivo, como tamaño, fecha de creación, etc.
 console.log(
-    (`Fecha de creación: ${stats.isFile}`), // fecha de creación del archivo
-    (`Es un enlace?: ${stats.isSymbolicLink}`), // Si es un enlace
-    (`¿Es un directorio?: ${stats.isDirectory()}`), // verifica si es un directorio
-    (`Tamaño del archivo: ${stats.size} bytes`),
+  (`Fecha de creación: ${stats.isFile}`), // fecha de creación del archivo
+  (`Es un enlace?: ${stats.isSymbolicLink}`), // Si es un enlace
+  (`¿Es un directorio?: ${stats.isDirectory()}`), // verifica si es un directorio
+  (`Tamaño del archivo: ${stats.size} bytes`)
 )
