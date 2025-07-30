89422f7bb8c290c5d829bb68b4da1b968abae640
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2025-07-31 00:44:53 +0200

    clase-2

---
+++ clase-1/9.http.js
@@ -0,0 +1,17 @@
+const http = require('node:http')
+const { findAvailablePort } = require('./10.free-port')
+
+console.log(process.env)
+
+const desiredPort = process.env.PORT ?? 3000
+
+const server = http.createServer((req, res) => {
+  console.log('request received')
+  res.end('<h1 style="color: red">Hola mundo</h1>')
+})
+
+findAvailablePort(desiredPort).then(port => {
+  server.listen(port, () => {
+    console.log(`Server is listening on port http://localhost:${port}`)
+  })
+})
